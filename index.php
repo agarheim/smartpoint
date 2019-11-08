8c68e39d290632e5a42baf5e9e3d4abbcf4073be
romanr <agarheim@gmail.com>
2019-11-08 15:51:10 +0200

    #

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<?php
-
-
-?>
 <html>
 <head>
     <title>test page - get api key</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="container ">
